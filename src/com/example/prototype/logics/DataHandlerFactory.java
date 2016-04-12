438fdbb1a843a2ac592a0ae036501145664208e2
avalanchas <da.heuschkel@gmail.com>
2016-04-12 18:10:40 +0200

    added recordings

---
+++ src/com/example/prototype/logics/DataHandlerFactory.java
@@ -0,0 +1,30 @@
+package com.example.prototype.logics;
+
+/**
+ * Simple class factory with a builder method to generate a specific DataHandler. The factory is currently very
+ * simplistic and generates the instance according to the string passed to the builder method
+ * <p/>
+ * This may be extended in the future to scan the incoming data for content and then select and appropriate handler upon
+ * detection with a degree of certainty
+ * <p/>
+ * Created by Patrick
+ */
+public class DataHandlerFactory {
+
+    /**
+     * Finds a handler instance appropriate to the type that is passed as a parameter
+     *
+     * @param type The type of handler to select
+     * @return The DataHandler instance that is appropriate for the type of data
+     */
+    public DataHandler getHandlerInstance(String type) {
+        switch (type) {
+            case "image":
+                return new BitmapHandler();
+            case "text":
+                return new TextHandler();
+            default:
+                return new TextHandler();
+        }
+    }
+}
--- src/com/example/prototype/ui/ActivityFactory.java
+++ src/com/example/prototype/ui/ActivityFactory.java
@@ -2,13 +2,13 @@ package com.example.prototype.ui;
 
 import com.example.prototype.DataKeys;
 import com.example.prototype.logics.DataHandler;
-import com.example.prototype.logics.HandlerFactory;
+import com.example.prototype.logics.DataHandlerFactory;
 
 import java.io.File;
 import java.util.List;
 
 /**
- * Activity for handling file data in a more advanced manner. A {@link HandlerFactory} will be invoked to find a
+ * Activity for handling file data in a more advanced manner. A {@link DataHandlerFactory} will be invoked to find a
  * befitting implementation for the handling. That implementation will then process the files <b>specialised onto that
  * particular situation</b>. The processed data will then be laid onto a <b>specialised</b> view, decided also by the
  * {@link DataHandler} implementation! The data handler implementation may return any child class of {@link
@@ -22,7 +22,7 @@ public class ActivityFactory extends PrototypeActivity {
     @Override
     public void inputFound(List<File> data) {
         // At this point, file scanning could be implemented to get better guesses of what the input actually is
-        DataHandler handler = HandlerFactory.getHandlerInstance(DataKeys.TYPE);
+        DataHandler handler = new DataHandlerFactory().getHandlerInstance(DataKeys.TYPE);
         for (File file : data) {
             // Currently, simply take the data and pass it on to whichever implementation the DataKey.TYPE specifies
             mHost.addView(prepareView(handler.handleData(this, file)));
